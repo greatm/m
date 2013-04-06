28f3ec5c9f37be9217ee5d0c298830b7e8dfd8c0
greatm <manoj.great@yahoo.com>
2013-04-06 12:50:48 +0530

    add_all_04/06_12_37

---
+++ m/Controllers/MasterUsersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace m.Controllers
+{
+    public class MasterUsersController : Controller
+    {
+        //
+        // GET: /MasterUsers/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
