e8cd112985b7c829550215db21ad2e5e978b7a6a
greatm <manoj.great@yahoo.com>
2013-04-10 16:26:33 +0530

    add_all_04/10_6_17

---
+++ m/migrations/201304101041198_blank.cs
@@ -0,0 +1,16 @@
+namespace m.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class blank : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
