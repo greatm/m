1d627d6ed1591f207513b87d9f5df4c7996084f0
greatm <manoj.great@yahoo.com>
2013-04-10 11:51:04 +0530

    add_all_04/10_1_37

--- m/migrations/201304091214099_employee_user.cs
+++ m/migrations/201304091214099_employee_user.cs
@@ -2,14 +2,14 @@ namespace m.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class employee_user : DbMigration
     {
         public override void Up()
         {
             AddColumn("dbo.Employees", "UserName", c => c.String());
         }
-        
+
         public override void Down()
         {
             DropColumn("dbo.Employees", "UserName");
