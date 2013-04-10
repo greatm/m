752bdfac6130acd8621fc103853552843ab0b70c
greatm <manoj.great@yahoo.com>
2013-04-10 14:19:33 +0530

    add_all_04/10_4_12

--- m/Filters/InitializeSimpleMembershipAttribute.cs
+++ m/Filters/InitializeSimpleMembershipAttribute.cs
@@ -39,8 +39,8 @@ namespace m.Filters
                     }
 
                     //WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
-                 
-                    if(!WebSecurity.Initialized)                    WebSecurity.InitializeDatabaseConnection("mDBContext", "Employees", "ID", "UserName", autoCreateTables: true);
+
+                    if (!WebSecurity.Initialized) WebSecurity.InitializeDatabaseConnection("mDBContext", "Employees", "ID", "UserName", autoCreateTables: true);
                 }
                 catch (Exception ex)
                 {
